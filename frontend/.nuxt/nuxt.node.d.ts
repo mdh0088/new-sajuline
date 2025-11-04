acd3fbef83addd208b7fc2adb1041e4012a7f64b
poyi123 <mdh0088@gmail.com>
2025-11-04 16:38:25 +0900

    frontend 배포 테스트

---
+++ frontend/.nuxt/nuxt.node.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="@nuxt/scripts" />
+/// <reference types="@vueuse/nuxt" />
+/// <reference types="@nuxt/image" />
+/// <reference types="@nuxt/icon" />
+/// <reference types="@nuxtjs/tailwindcss" />
+/// <reference types="@pinia/nuxt" />
+/// <reference types="@nuxt/fonts" />
+/// <reference types="@element-plus/nuxt" />
+/// <reference types="notivue" />
+/// <reference types="@nuxt/devtools" />
+/// <reference types="@nuxt/telemetry" />
+/// <reference path="types/modules.d.ts" />
+/// <reference path="types/runtime-config.d.ts" />
+/// <reference path="types/app.config.d.ts" />
+/// <reference types="nuxt" />
+/// <reference path="types/nitro-middleware.d.ts" />
+/// <reference path="schema/nuxt.schema.d.ts" />
+
+export {}
